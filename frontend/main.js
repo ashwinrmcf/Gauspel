b1a49a3c6e6c301c8a6866ead4fd67f413ec42cb
ashwinrmcf <ashwinrmcf@gmail.com>
2025-08-21 00:09:52 +0530

    Electron
    Electron setup

---
+++ frontend/main.js
@@ -0,0 +1,15 @@
+const { app, BrowserWindow } = require('electron');
+require('electron-reload')(__dirname);
+function createWindow () {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
